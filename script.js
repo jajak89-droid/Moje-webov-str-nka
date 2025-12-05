cd77dc86aa7b729e10045578b36ab5690a66e1ad
jajak89-droid <jajak89@gmail.com>
2025-12-05 19:23:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const btn = document.getElementById("themeToggle");
-btn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    btn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.getElementById('darkModeBtn');
+
+    btn.addEventListener('click', function() {
+        document.body.classList.toggle('dark');
+
+        if(document.body.classList.contains('dark')){
+            btn.textContent = "☀️ Světlý režim";
+        } else {
+            btn.textContent = "🌙 Ztmavit";
+        }
+    });
 });

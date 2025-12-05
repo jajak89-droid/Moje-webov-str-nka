9e40e1ff917dc2ba9aaf8a2c983a9559b79d6c2b
jajak89-droid <jajak89@gmail.com>
2025-12-05 17:07:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ btn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     btn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
 });
+.logo {
+    width: 200px; 
+    max-width: 80%;
+    display: block;
+    margin: 20px auto;
+}
